b70b1a00e495f74072b06ccba47a1988c2f95768
Reverseking1 <127144970+Reverseking1@users.noreply.github.com>
2024-11-11 18:23:09 +0000

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -2,24 +2,25 @@ const { smd, sleep } = require('../lib');
 
 smd({ cmdname: "hack", type: "fun", info: "hacking prank", filename: __filename }, async (citel) => {
   const messages = [
-    "Injecting Malware",
-    " █ 10%",
-    " █ █ 20%",
-    " █ █ █ 30%",
-    " █ █ █ █ 40%",
-    " █ █ █ █ █ 50%",
-    " █ █ █ █ █ █ 60%",
-    " █ █ █ █ █ █ █ 70%",
-    " █ █ █ █ █ █ █ █ 80%",
-    " █ █ █ █ █ █ █ █ █ 90%",
-    " █ █ █ █ █ █ █ █ █ █ 100%",
-    "System hyjacking on process.. \\n Conecting to Server error to find 404 ",
-    "Device successfully connected... \\n Riciving data...",
-    "Data hyjacked from divice 100% completed \\n killing all evidence killing all malwares...",
+    "Reverse King Injecting Malware",
+    " ▓ 10%",
+    " ▓ ▆ 20%",
+    " ▓ ▆ ▅ 30%",
+    " ▓ ▆ ▅ ▃ 40%",
+    " ▓ ▆ ▅ ▃ ▂ 50%",
+    " ▓ ▆ ▅ ▅ ▃ ▂ 60%",
+    " ▓ ▆ ▅ ▓ ▅ ▅ ▃  70%",
+    " ▓ ▆ ▅ ▓ ▓ ▆  ▃ ▂ 80%",
+    " ▓ ▆ ▓ ▆ ▓ ▓  ▆ ▃ ▂ 90%",
+    " ▓ ▆ ▆ ▓ ▓ ▓ ▆ ▅ ▃ ▂ ▁ 100%",
+    "System hijacking on process.. \\n Conecting to Reverse King to find 404 ",
+    "Device successfully connected... \\n Receiving data...",
+    "Data hijacked from device 100% completed \\n killing all evidence killing all malwares...",
     " HACKING COMPLETED ",
     " SENDING LOG DOCUMENTS...",
     " SUCCESSFULLY SENT DATA AND Connection disconnected",
     "BACKLOGS CLEARED"
+    "Reverse King responded anonymously
   ];
 
   let editedMessage;
